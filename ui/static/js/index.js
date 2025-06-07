f79c5b5d8f20a8d3008c415e47b22d628e01cc35
andyaspel <aspelandy@outlook.com>
2025-06-07 15:29:49 +0200

    saved progress

--- ui/static/js/index.js
+++ ui/static/js/index.js
@@ -1,8 +1,11 @@
 "use strict"
 
 console.log("hello");
-let a = 1
-let b = 2
-if (a  === b) {
-    return a
+let a = 1;
+let b = 2;
+if (a > b) {
+    console.log(a - b);
+}
+else {
+    console.log(b + a);
 }
